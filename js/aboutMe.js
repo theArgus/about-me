93fa9dce6c639372ee7d9c7d116a262d70419c0d
theArgus <fi.nhuerta@gmail.com>
2023-04-04 17:51:38 -0600

    Image remove, tittle change

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -7,7 +7,7 @@ function setup()
   canvas.style('z-index', '-1');
   canvas.style('opacity', '0.3');
   frameRate(12);
-  background(0);
+
   noCursor();
   angleMode(DEGREES);
 }
@@ -19,18 +19,22 @@ function draw()
   rectMode(CENTER);
   sphere(52);
   noFill();
-  stroke(255);
+  stroke(0);
   push();
   angle++;
   pop();
 
-  for (var i = 0; i < 177; i++)
+  for (var i = 0; i < 24; i++)
   {
     translate(i*13, i*-i)
-    stroke(7*i);
+    stroke(i,i*27,i+12)
     rotate(angle);
     rectMode(CENTER);
-    sphere(52);
+    sphere(52+random(25,i));
+    for (var j = 0; j < 14; j++){
+      sphere(i+random(25,j));
+      stroke(0,j,i*52)
+    }
   }
 
 }
